6378c25b77fdde2aae796b5c323c1e0ff070791e
Mohammad Shahgolzadeh <m2shm2sh@gmail.com>
2015-12-22 14:54:22 -0500

    Applied fixes from StyleCI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 
 
-if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
+if (!file_exists(__DIR__.'/../vendor/autoload.php')) {
     throw new \RuntimeException('Unable to locate autoloader. Install dependencies from the project root directory to run test suite: `composer install`.');
 }
 
-include __DIR__ . '/../vendor/autoload.php';
+include __DIR__.'/../vendor/autoload.php';
